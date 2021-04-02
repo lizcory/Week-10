8baede07be7619ad8a6f6b1280a48fb49fba4a2e
work <Work@Navarjuns-MBP.hitronhub.home>
2021-04-02 06:23:42 -0700

    adding missing files

---
+++ pie/scripts/Donut.js
@@ -0,0 +1,93 @@
+function Donut() {
+
+    this._data = null;
+    this._sel = null;
+    this._colorScale = null;
+    this._padding = 15;
+    this._size = 180;
+    this._radius = () => { return this._size/2 - this._padding };
+
+    this.data = function () {
+        if (arguments.length > 0) {
+            this._data = arguments[0];
+            return this;
+        }
+        return this._data;
+    };
+
+    this.selection = function () {
+        if (arguments.length > 0) {
+            this._sel = arguments[0];
+            return this;
+        }
+        return this._sel;
+    };
+
+    this.colorScale = function () {
+        if (arguments.length > 0) {
+            this._colorScale = arguments[0];
+            return this;
+        }
+        return this._colorScale;
+    };
+
+    this.padding = function () {
+        if (arguments.length > 0) {
+            this._padding = arguments[0];
+            return this;
+        }
+        return this._padding;
+    };
+
+    this.size = function () {
+        if (arguments.length > 0) {
+            this._size = arguments[0];
+            return this;
+        }
+        return this._size;
+    };
+
+    this.draw = function () {
+        // create a pie layout
+        let pie = d3.pie()
+            .sort(d => d.key)
+            .value(d => d.value);
+
+        // create arc function
+        let arc = d3.arc()
+            .innerRadius(0)
+            .outerRadius(this._radius);
+
+        let layout = pie(this._data.arr);
+
+        let g = this._sel
+            .selectAll('g')
+            .data(layout)
+            .join('g');
+        g.selectAll('path')
+            .data(d => [d])
+            .join('path')
+            .attr('d', arc)
+            .attr('fill', d => this._colorScale(d.data.key));
+        
+        // g.selectAll('text')
+        //     .data(d => [d])
+        //     .join('text')
+        //     .attr('transform', d => {
+        //         let centroid = arc.centroid(d);
+        //         return `translate(${centroid[0]}, ${centroid[1]})`;
+        //     })
+        //     .attr('text-anchor', 'middle')
+        //     .text(d => d.data.key);
+        
+        this._sel
+            .selectAll('text.title')
+            .data([this._data.name])
+            .join('text')
+            .classed('title', true)
+            .text(d => d);
+        
+    }
+
+    return this;
+}
---
+++ pie/scripts/script.js
@@ -0,0 +1,55 @@
+const margin = {t: 50, r:50, b: 50, l: 50};
+const padding = 15;
+const size = {w: 1000, h: 800};
+const svg = d3.select('svg#pie');
+
+svg.attr('width', size.w)
+    .attr('height', size.h);
+
+const containerG = svg.append('g')
+    .classed('container', true)
+    .attr('transform', `translate(${margin.t}, ${margin.l})`);
+
+size.w = size.w - margin.l - margin.r;
+size.h = size.h - margin.t - margin.b;
+
+const columns = ['hp', 'speed', 'attack', 'defense', 'spAtk', 'spDef'];
+const colorScale = d3.scaleOrdinal()
+    .domain(columns)
+    .range(d3.schemeSet2);
+
+d3.csv('data/Pokemon_subset.csv', function(d) {
+    d.total = +d.total;
+    d.hp = +d.hp;
+    d.attack = +d.attack;
+    d.defense = +d.defense;
+    d.spAtk = +d.spAtk;
+    d.spDef = +d.spDef;
+    d.speed = +d.speed;
+    // creating an array of all values for later convenience
+    d.arr = [
+        {key: 'hp', value: d.hp},
+        {key: 'attack', value: d.attack},
+        {key: 'defense', value: d.defense},
+        {key: 'spAtk', value: d.spAtk},
+        {key: 'spDef', value: d.spDef},
+        {key: 'speed', value: d.speed}
+    ];
+    return d;
+})
+.then(function(data) {
+    data = data.sort((a, b) => a.total < b.total);
+});
+
+function positionPieChart(index) {
+    let radius = size.w/10 - padding;
+    let m = index*2 + 1; // multiplier
+
+    let x = (radius + padding) * m;
+    m = Math.floor(x/size.w)*2 + 1;
+    
+    let y = (radius + padding) * m;
+    x = x - (Math.floor(x/size.w) * size.w);
+
+    return [x, y];
+}
